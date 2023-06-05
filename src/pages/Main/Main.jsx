e9f7720645157affaa52b317f67c68acef33c082
ChanwooPark <chanw9503@gmail.com>
2023-06-06 00:22:14 +0900

    ✨ 스크롤 이벤트 추가

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Gear from '../../components/Gear/Gear';
 import { styled } from 'styled-components';
 
 const Main = () => {
+  const targetRef = useRef(null);
+  const scrollHandler = () => {
+    console.log('scrolling');
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      window.addEventListener('scroll', scrollHandler);
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
   return (
-    <GearBlock>
-      <Gear />
-    </GearBlock>
+    <div style={{ height: '2000px' }}>
+      <GearBlock>
+        <Gear />
+      </GearBlock>
+    </div>
   );
 };
 
